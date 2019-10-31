7f08d4913f525097e9ae66fe1d14c7b4931c7bb5
Nam Kyo <nam@namkyo.jp>
2019-10-31 11:38:55 +0700

    Update caidat.sh

--- caidat.sh
+++ caidat.sh
@@ -11,6 +11,8 @@ cpufreq=$( awk -F: ' /cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo )
 svram=$( free -m | awk 'NR==2 {print $2}' )
 svhdd=$( df -h | awk 'NR==2 {print $2}' )
 svswap=$( free -m | awk 'NR==4 {print $2}' )
+
+
 echo "=========================================================================="
 echo "Thong Tin Server:  "
 echo "---------------------------------------------------------------------------"
@@ -49,6 +51,9 @@ systemctl enable clamd@scan
 ######cai dat ssmtp & mailx
 yum install ssmtp mailx
 ##########cau hinh ssmtp
+
+echo -n "Nhap ten mien (Vi du: matbao.com) :  "
+read tenmien
 echo -n "Nhap server SMTP (Vi du: smtp.gmail.com:465) :  "
 read sv_smtp
 echo -n "Nhap user SMTP(Vi du: admin@matbao.com): "
@@ -59,6 +64,8 @@ read smtp_pass
 clear
 echo "========================================================================="
 echo "Thong Tin Ban Da Nhap:"
+echo "========================================================================="
+echo "Ten mien: $tenmien"
 echo "========================================================================="
 echo "Server SMTP: $sv_smtp"
 echo "-------------------------------------------------------------------------"
@@ -87,7 +94,7 @@ yum -y remove postfix*
 
 clear
 printf "=========================================================================\n"
-printf "Cai Dat Hoan Tat, Bat Dau Qua Trinh Cau Hinh... \n"
+printf "Cai Dat Hoan Tat, Vui long nhap cau hinh ket noi SMTP... \n"
 printf "=========================================================================\n"
 sleep 3
 
@@ -110,51 +117,21 @@ TLS_CA_File=/etc/pki/tls/certs/ca-bundle.crt
 END
 ##cau hinh file Quet virus
 mkdir /var/log/clamav
-cat > "./scan.sh" <<END
-#!/bin/bash
-#
-LOGFILE="/var/log/clamav/clamav-log-$(TZ=Asia/Ho_Chi_Minh date +'%Y-%m-%d_%H-%M-%S').log"; 
-FOUNDFILE="/var/log/clamav/clamav-found-$(TZ=Asia/Ho_Chi_Minh date +'%Y-%m-%d_%H-%M-%S').log"; 
-SVIP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}');
-EMAIL_MSG="Kính thưa Quý khách,
-Hệ thống giám sát bảo mật trên máy chủ "$svip" được đăng kí tại Mắt Bão đã phát hiện một (hoặc nhiều) tập tin có khả năng bị nhiễm mã độc (còn gọi là malware, virus).
-Hiện tại, những tập tin không an toàn đã được phần mềm bảo mật ClamAV tự động ngăn chặn thực thi nhằm đảm bảo an toàn kịp thời cho hệ thống và dữ liệu đang vận hành trên máy chủ "$svip".
-Quý khách vui lòng kiểm tra và loại bỏ những đoạn mã độc (hoặc tập tin) này để máy chủ tiếp tục hoạt động an toàn. Quý khách có thể xem hướng dẫn xử lý mã độc để loại bỏ toàn bộ mã độc trên website tại liên kết sau:
-https://wiki.matbao.net/kb/huong-dan-xu-ly-ma-doc
-Sau đây là danh sách những tập tin được phát hiện bị nhiễm mã độc trên server "$SVIP" cần Quý khách kiểm tra và khắc phục nhanh chóng:"; 
-EMAIL_FROM="abuse@web.root.dns.server.namkyo.jp";
-EMAIL_TO="nam@dam.vc";
-DIRTOSCAN="/home";
+wget https://raw.githubusercontent.com/nkyo/clamAV/master/scan.sh
+chmod +x scan.sh
+echo -n "Nhap dia chi mail nhan thong bao :  "
+read MAILDEN
+echo -n "Thu muc scan (Vi du: home) :  "
+read SUATHUMUCSCAN
+sed -i -e "s/MAILDI/$smtp_user/" scan.sh
+sed -i -e "s/MAILDEN/$MAILDEN/" scan.sh
+sed -i -e "s/SUATHUMUCSCAN/$SUATHUMUCSCAN/" scan.sh
+echo "Quá trình cài đặt ClamAV đã hoàn tất trên máy chủ $svip " | mail -v -s "[THÔNG BÁO] Cài đặt ClamAV hoàn tất" "$MAILDEN"
+clear
+printf "=========================================================================\n"
+printf "Cai Dat Hoan Tat\n"
+printf "=========================================================================\n"
 
-# Update ClamAV database
-echo "Dang cap nhat CSDL virus...";
-freshclam --quiet;
 
-TODAY=$(date +%u);
-
-if [ "$TODAY" == "1" ];then
- echo "Bat dau quet virus.";
-
- # be nice to others while scanning the entire root
- nice -n5 clamscan -ri / --exclude-dir=/sys/ &>"$LOGFILE";
-else
- DIRSIZE=$(du -sh "$DIRTOSCAN" 2>/dev/null | cut -f1);
-
- echo "Bat dau quet virus hang ngay thu muc "$DIRTOSCAN"
- dung luong file can quet la "$DIRSIZE".";
-
- clamscan -ri "$DIRTOSCAN" &>"$LOGFILE";
-fi
-
-MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3); 
-FOUND=$(grep FOUND $LOGFILE >> $FOUNDFILE );
-MADOC=$(sed -i -e 's/:/ - Loại mã độc - /' $FOUNDFILE );
-MADOC=$(sed -i -e 's/FOUND/ /' $FOUNDFILE );
-MADOC=$(sed -i 's/^/Tập tin: /' $FOUNDFILE );
-if [ "$MALWARE" -ne "0" ];then 
-  echo -e "$EMAIL_MSG \n $(cat $FOUNDFILE)"|mail -a "$FOUNDFILE" -s "[CẢNH BÁO NGUY HIỂM]Phát hiện các tập tin có chứa virus trên máy chủ "$SVIP"" -r "$EMAIL_FROM" "$EMAIL_TO";
-fi 
-exit 0
-END
 exit
 ;;
