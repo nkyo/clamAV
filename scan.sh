0af993b02c03c70a2545825fb2508492ecad6f81
Nam Kyo <nam@namkyo.jp>
2019-11-19 15:34:50 +0700

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,17 +3,17 @@
 LOGFILE="/var/log/clamav/clamav-log-$(TZ=Asia/Ho_Chi_Minh date +'%Y-%m-%d_%H-%M-%S').log"; 
 FOUNDFILE="/var/log/clamav/clamav-found-$(TZ=Asia/Ho_Chi_Minh date +'%Y-%m-%d_%H-%M-%S').log"; 
 SVIP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}');
-EMAIL_MSG="Kính thưa Quý khách,
+EMAIL_MSG="Kinh thua Quy khach,
 
-Hệ thống giám sát bảo mật trên máy chủ "$svip" được đăng kí tại Mắt Bão đã phát hiện một (hoặc nhiều) tập tin có khả năng bị nhiễm mã độc (còn gọi là malware, virus).
+He thong giam sat bao mat tren may chu "$svip" duoc dang ki tai Mat Bao da phat hien mot (hoac nhieu) tap tin co kha nang bi nhiem ma doc (con goi la malware, virus).
 
-Hiện tại, những tập tin không an toàn đã được phần mềm bảo mật ClamAV tự động ngăn chặn thực thi nhằm đảm bảo an toàn kịp thời cho hệ thống và dữ liệu đang vận hành trên máy chủ "$svip".
+Hien tai, nhung tap tin khong an toan da duoc phan mem bao mat ClamAV tu dong ngan chan thuc thi nham dam bao an toan kip thoi cho he thong va du lieu dang van hanh tren may chu "$svip".
 
-Quý khách vui lòng kiểm tra và loại bỏ những đoạn mã độc (hoặc tập tin) này để máy chủ tiếp tục hoạt động an toàn. Quý khách có thể xem hướng dẫn xử lý mã độc để loại bỏ toàn bộ mã độc trên website tại liên kết sau:
+Quy khach vui long kiem tra va loai bo nhung doan ma doc (hoac tap tin) nay de may chu tiep tuc hoat dong an toan. Quy khach co the xem huong dan xu ly ma doc de loai bo toan bo ma doc tren website tai lien ket sau:
 
 https://wiki.matbao.net/kb/huong-dan-xu-ly-ma-doc
 
-Sau đây là danh sách những tập tin được phát hiện bị nhiễm mã độc trên server "$SVIP" cần Quý khách kiểm tra và khắc phục nhanh chóng:"; 
+Sau day la danh sach nhung tap tin duoc phat hien bi nhiem ma doc tren server "$SVIP" can Quy khach kiem tra va khac phuc nhanh chong:"; 
 EMAIL_FROM="MAILDI";
 EMAIL_TO="MAILDEN";
 DIRTOSCAN="/SUATHUMUCSCAN";
@@ -40,10 +40,10 @@ fi
 
 MALWARE=$(tail "$LOGFILE"|grep Infected|cut -d" " -f3); 
 FOUND=$(grep FOUND $LOGFILE >> $FOUNDFILE );
-MADOC=$(sed -i -e 's/:/ - Loại mã độc - /' $FOUNDFILE );
+MADOC=$(sed -i -e 's/:/ - Loai ma doc - /' $FOUNDFILE );
 MADOC=$(sed -i -e 's/FOUND/ /' $FOUNDFILE );
-MADOC=$(sed -i 's/^/Tập tin: /' $FOUNDFILE );
+MADOC=$(sed -i 's/^/Tap tin: /' $FOUNDFILE );
 if [ "$MALWARE" -ne "0" ];then 
-  echo -e "$EMAIL_MSG \n $(cat $FOUNDFILE)"|mail -a "$FOUNDFILE" -s "[CẢNH BÁO NGUY HIỂM]Phát hiện các tập tin có chứa virus trên máy chủ "$SVIP"" -r "$EMAIL_FROM" "$EMAIL_TO";
+  echo -e "$EMAIL_MSG \n $(cat $FOUNDFILE)"|mail -a "$FOUNDFILE" -s "[CANH BAO NGUY HIEM]Phat hien cac tap tin co chua virus tren may chu "$SVIP"" -r "$EMAIL_FROM" "$EMAIL_TO";
 fi 
 exit 0
